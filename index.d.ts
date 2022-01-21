77e223e7a2d8ce5b1cb5e150286de76e12949912
Ruslan Zapevalov <upacyxou@tutanota.com>
2022-01-21 16:36:46 +0400

    fix array of translations

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 import { Context, ComponentType, Consumer, PropsWithChildren } from "react";
 
 interface Translations {
-  [key: string]: string | number | Translations;
+  [key: string]: string | number | Translations | Translations[];
 }
 
 type TemplateValues = Record<string, string | number>;
